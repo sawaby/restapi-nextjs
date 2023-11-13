758c2ed991828fa669f4b7395f0566e1c2c01f37
Maria <sawaby@users.noreply.github.com>
2023-11-13 09:37:14 -0800

    mutation

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,81 +1,81 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
-const API_KEY: string = process.env.DATA_API_KEY as string
-export async function GET(request: Request){
-    const origin = request.headers.get('origin')
-    const res = await fetch(DATA_SOURCE_URL)
+// const DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/todos"
+// const API_KEY: string = process.env.DATA_API_KEY as string
+// export async function GET(request: Request){
+//     const origin = request.headers.get('origin')
+//     const res = await fetch(DATA_SOURCE_URL)
 
-    const todos: Todo[] = await res.json();
+//     const todos: Todo[] = await res.json();
 
-    return new NextResponse(JSON.stringify(todos), {
-        headers: {
-            'Access-Control-Allow-Origin': origin || "*",
-            'Content-Type': 'application/json'
-        }
-    });
-}
+//     return new NextResponse(JSON.stringify(todos), {
+//         headers: {
+//             'Access-Control-Allow-Origin': origin || "*",
+//             'Content-Type': 'application/json'
+//         }
+//     });
+// }
 
-export async function DELETE(request: Request){
-    const { id }: Partial<Todo> = await request.json();
+// export async function DELETE(request: Request){
+//     const { id }: Partial<Todo> = await request.json();
 
-    if (!id) return NextResponse.json({ "message": "Todo id required" });
+//     if (!id) return NextResponse.json({ "message": "Todo id required" });
 
-    await fetch(`${DATA_SOURCE_URL}/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'API_KEY': API_KEY,
+//     await fetch(`${DATA_SOURCE_URL}/${id}`, {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'API_KEY': API_KEY,
 
-        }
-    });
+//         }
+//     });
 
-    return NextResponse.json( { "message": `Todo ${id} deleted`})
-}
+//     return NextResponse.json( { "message": `Todo ${id} deleted`})
+// }
 
-export async function POST(request: Request){
-    const { userId, title }: Partial<Todo> = await request.json();
+// export async function POST(request: Request){
+//     const { userId, title }: Partial<Todo> = await request.json();
 
-    if (!userId || !title) return NextResponse.json({ "message": "missing required data" });
+//     if (!userId || !title) return NextResponse.json({ "message": "missing required data" });
 
-    const res = await fetch(DATA_SOURCE_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'API_KEY': API_KEY,
+//     const res = await fetch(DATA_SOURCE_URL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'API_KEY': API_KEY,
 
-        },
-        body: JSON.stringify({
-            userId, title, comleted: false
-        })
-    });
+//         },
+//         body: JSON.stringify({
+//             userId, title, comleted: false
+//         })
+//     });
 
-    const newTodo: Todo = await res.json();
-
-
-    return NextResponse.json(newTodo)
-}
+//     const newTodo: Todo = await res.json();
 
 
-export async function PUT(request: Request){
-    const { userId, id, title, completed }: Todo = await request.json();
-
-    if (!userId || !id || !title || typeof(completed) !== 'boolean') return NextResponse.json({ "message": "missing required data" });
-
-    const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'API_KEY': API_KEY,
-
-        },
-        body: JSON.stringify({
-            userId, title, completed
-        })
-    });
-
-    const updatedTodo: Todo = await res.json();
+//     return NextResponse.json(newTodo)
+// }
 
 
-    return NextResponse.json(updatedTodo)
-}
+// export async function PUT(request: Request){
+//     const { userId, id, title, completed }: Todo = await request.json();
+
+//     if (!userId || !id || !title || typeof(completed) !== 'boolean') return NextResponse.json({ "message": "missing required data" });
+
+//     const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'API_KEY': API_KEY,
+
+//         },
+//         body: JSON.stringify({
+//             userId, title, completed
+//         })
+//     });
+
+//     const updatedTodo: Todo = await res.json();
+
+
+//     return NextResponse.json(updatedTodo)
+// }
